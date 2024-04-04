7477c2bee41db072fc71c590d3bba54c27f63fd1
jeffer-thebandit <jeff39724@gmail.com>
2024-04-04 15:17:02 -0700

    deploying using fabric

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["54.82.215.66", "54.172.81.186"]
 
 
 def do_pack():
